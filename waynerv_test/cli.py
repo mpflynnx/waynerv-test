fc719723d65b16c9cadb01417ec199c5c0d0e9f7
Slackware Live User <live@ebay2021.example.net>
2022-01-17 16:40:07 +0000

    commit for tag v0.l.1

--- waynerv_test/cli.py
+++ waynerv_test/cli.py
@@ -10,6 +10,10 @@ def main():
     click.echo("=" * len("waynerv-test"))
     click.echo("Skeleton project created by Cookiecutter PyPackage")
 
+# Just some random text for next version bump tag 0.1.1
+
+
+
 
 if __name__ == "__main__":
     main()  # pragma: no cover
